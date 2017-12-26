ae341c9f4c0a9210e494e24a113fac4e3a617b9d
Murat YARAL <muratyaral@gmail.com>
2017-12-26 05:33:58 +0300

    add project page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req,res)=>{
+    res.render('projects',{
+        pageTitle: 'Projects'
+    });
+});
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Sunucu Aktif');
